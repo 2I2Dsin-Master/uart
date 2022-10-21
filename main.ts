521ec5af87088dd262acf2c6492d9e2566709303
SurfeurDeCuivre <83498613+2I2Dsin-Master@users.noreply.github.com>
2022-10-21 20:09:40 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    serial.writeString("- A -")
+})
+input.onButtonPressed(Button.B, function () {
+    serial.writeString("--B--")
+})
 serial.redirect(
 SerialPin.P8,
 SerialPin.P12,
 BaudRate.BaudRate9600
 )
+basic.pause(200)
+basic.showIcon(IconNames.Sword)
+serial.writeString("HELLO")
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
-    serial.writeString("HELLO")
-    basic.pause(15)
+	
 })
